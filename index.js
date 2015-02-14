bf04fc13179e29d5b2fd4ad34b417a8a7339c7db
Scott O'Neal <scottmod@gmail.com>
2015-02-14 16:16:09 -0500

    Not a final patch, just a start to semi rewrite. Added basic tests.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var fs    = require('fs')
   , spawn = require('child_process').spawn
   , temp  = require('temp');
 
-exports.generateFdf = function(data, fileName) {
+exports.generateFdf = function(data) {
   var header, body, footer, dataKeys;
+
+  // We should really come up with a new way of making FDF files, this is doing a lot of buffer instantiation..
 
   header = Buffer([]);
   header = Buffer.concat([ header, new Buffer("%FDF-1.2\n") ]);
@@ -51,13 +53,16 @@ exports.generateFdf = function(data, fileName) {
   return fdf;
 }
 
-exports.generatePdf = function(data, templatePath,callback) {
+exports.generatePdf = function(data, templatePath, callback) {
   var tempName = temp.path({suffix: '.pdf'});
   var tempNameResult = temp.path({suffix: '.pdf'});
 
-  child = spawn("pdftk", ["app/"+templatePath,"fill_form","-","output",tempName,"flatten"]);
+  child = spawn("pdftk", [__dirname+templatePath,"fill_form","-","output",tempName,"flatten"]);
 
   child.on('exit', function(code) {
+
+    // Is GhostScript necessary for this module?
+    /*
     var cmd = "gs -dNOCACHE -sDEVICE=pdfwrite -sOutputFile="+tempNameResult +" -dbatch -dNOPAUSE -dQUIET  " + tempName +"  -c quit"
     console.log(cmd);
     exec(cmd,  function (error, stdout, stderr) {
@@ -77,9 +82,21 @@ exports.generatePdf = function(data, templatePath,callback) {
       callback(result);
      });
    });
+   */
 
+    // Throw Error if code is given.. Code = 0 is good
+    if ( code ) {
+      throw new Error('Exit Code: ' + code);
+    }
+
+    // Return Temp File Path, this will allow the user to either create an async or sync stream, which they then can unlink.
+    callback(tempName);
+  });
+
+  // Ideally this would be a piped, instead of a single write..
   child.stdin.write(exports.generateFdf(data));
   child.stdin.end();
+
   child.on('error', function (err) {
     console.log('Errror', err);
   });
